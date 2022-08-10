5347759dfda597734b9b4b3cbf3be3e63bf8f16c
andrei_zverev <andreyzverev1987@gmail.com>
2022-08-10 20:46:22 +0500

    Входные данные в тесте [#297750 #330812]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -19,6 +19,7 @@ public class X2Test {
         assertEquals(expected, result);
     }
 
+    @Test
     public void whenA1B1C1X1Then3() {
         int a = 1;
         int b = 1;
@@ -31,6 +32,7 @@ public class X2Test {
         assertEquals(expected, result);
     }
 
+    @Test
     public void whenA0B1C1X1Then2() {
         int a = 0;
         int b = 1;
@@ -43,6 +45,7 @@ public class X2Test {
         assertEquals(expected, result);
     }
 
+    @Test
     public void whenA1B1C0X1Then2() {
         int a = 0;
         int b = 1;
@@ -55,13 +58,14 @@ public class X2Test {
         assertEquals(expected, result);
     }
 
+    @Test
     public void whenA1B1C1X0Then3() {
         int a = 1;
         int b = 1;
         int c = 1;
         int x = 0;
 
-        int expected = 3;
+        int expected = 1;
         int result = X2.calc(a, b, c, x);
 
         assertEquals(expected, result);
