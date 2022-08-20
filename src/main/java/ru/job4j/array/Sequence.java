92ace296c66b9fd21339595d1fb6297e48a52a07
andrei_zverev <andreyzverev1987@gmail.com>
2022-08-20 12:45:22 +0500

    6.6.0. Двумерный массив [#226796 #334190]

--- src/main/java/ru/job4j/array/Sequence.java
+++ src/main/java/ru/job4j/array/Sequence.java
@@ -6,18 +6,11 @@ public class Sequence {
         array[0][0] = 2;
         array[0][1] = 3;
         array[0][2] = 7;
-        array[0][3] = 9;
         array[1][0] = 4;
         array[1][1] = 3;
         array[1][2] = 0;
-        array[1][3] = 3;
         array[2][0] = 2;
         array[2][1] = 7;
         array[2][2] = 8;
-        array[2][3] = 2;
-        array[3][0] = 3;
-        array[3][1] = 5;
-        array[3][2] = 7;
-        array[3][3] = 1;
     }
 }
