497eab98a479534cd3913d08d65ffdf9edcd276d
Зверев Андрей Алексеевич <zverevaa@mfcto.ru>
2022-09-07 11:47:37 +0500

    Рефакторинг - Расстояние между точками.

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -5,10 +5,11 @@ public class Point {
     private int x;
     private int y;
 
-    public Point(int x, int y){
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
+
     public double distance(Point that) {
         return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
     }
